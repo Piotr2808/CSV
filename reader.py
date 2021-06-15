48882498f704cb4c2f05e66dbca1ee76ebcdccaf
Piotr <piotr_jasnowski@o2.pl>
2021-06-15 11:38:50 +0200

    Koniec zadania i pliki do zadania

--- reader.py
+++ reader.py
@@ -1,16 +1,3 @@
-# Napisz program "reader.py", który zmodyfikuje plik csv i wyświetli
-# w terminalu jego zawartość, a następnie zapisze w wybranej lokalizacji.
-# Wykonanie programu:
-
-# reader.py <src> <dst> <change1> <change2> ..
-# src to ścieżka pliku csv. Jeśli plik nie istnieje bądź podana ścieżka nie jest
-# plikiem, wyświetl błąd i wskaż pliki w tym samym katalogu.
-# dst to docelowa ścieżka, w której zapiszemy zmieniony plik csv.
-
-# change1 ... changeN to ciągi znaków w posraci "Y,X,wartosc" Y to wiersz do zmodyfikowania
-# (liczony od 0, X to kolumna (liczona od 0).
-# Komórka pod wskazanym adresem powinna zmienić wartość na "wartosc"
-
 import sys
 
 class CsvReader:
@@ -18,21 +5,32 @@ class CsvReader:
         self.input_file = sys.argv[1]
         self.output_file = sys.argv[2]
         self.task = []
+        self.lines = self.read_file()
         for row in sys.argv[3:]:
-            self.task.append(row)
+            self.task.append(row.split(','))
     def read_file(self):
         lines = []
-        with open("plik.csv", "r", encoding='utf-8') as file:
+        with open(self.input_file, "r", encoding='utf-8') as file:
             for line in file.readlines():
                 tmp = line.split('\n')[0].split(',')
                 lines.append(tmp)
-            print(lines)
 
         return lines
 
     def change_file(self):
         for change in self.task:
+            self.lines[int(change[0])][int(change[1])] = change[2]
 
+    def save_file(self):
+        with open(self.output_file, "w", encoding='utf-8') as file_to_save:
+            for line in self.lines:
+                for element in line[:-1]:
+                    file_to_save.write(str(element) + ",")
+                file_to_save.write(str(line[-1] + "\n"))
 
 read = CsvReader()
 read.read_file()
+print(read.lines)
+read.change_file()
+print(read.lines)
+read.save_file()
